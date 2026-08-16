541c3296f38796add31d426a3040a3b5da32e031
Satwik Priyansh <satwikstrauss@gmail.com>
2026-08-16 08:58:10 +0530

    user_handlers moudle completed.

--- handlers/user_handlers.go
+++ handlers/user_handlers.go
@@ -1,1 +1,45 @@
 package handlers
+
+import (
+	"chess-puzzles-api/auth"
+	"chess-puzzles-api/store"
+	"encoding/json"
+	"errors"
+	"log/slog"
+	"net/http"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func HandleGetProfile(pool *pgxpool.Pool) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		userID, ok := ctx.Value(auth.UserIDKey).(string)
+		if !ok {
+			slog.Error("invalid user id")
+			http.Error(w, "internal server error", http.StatusUnauthorized)
+			return
+		}
+		user, err := store.GetUserByID(ctx, pool, userID)
+		if err != nil {
+			if errors.Is(err, pgx.ErrNoRows) {
+				slog.Error("no user found with the given id:", "id", userID, "error", err)
+				http.Error(w, "user not found", http.StatusNotFound)
+				return
+			}
+			slog.Error("internal server error:", "error", err)
+			http.Error(w, "internal server error", http.StatusInternalServerError)
+			return
+
+		}
+		w.Header().Set("Content-Type", "application/json")
+		err = json.NewEncoder(w).Encode(user)
+		if err != nil {
+			slog.Error("internal server error", "error", err)
+			http.Error(w, "internal server error", http.StatusInternalServerError)
+			return
+		}
+
+	}
+}
